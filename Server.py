b0cf5f6b62b85aa87cd4562ffa229d1bddb16d95
Nam Mai <mdnam2410@gmail.com>
2021-05-03 11:16:09 +0700

    Add codes for socket

--- Server.py
+++ Server.py
@@ -3,42 +3,52 @@ import Util
 
 import socket
 
-# SERVER_PORT = "TO BE DETERMINED"
+SERVER_PORT = 9098
 # SERVER_ADDRESS = socket.gethostbyname(socket.gethostname())
+SERVER_ADDRESS = '127.0.0.1'
 
-# socket = socket.socket()
-# socket.bind((SERVER_ADDRESS, SERVER_PORT))
+socket = socket.socket()
+socket.bind((SERVER_ADDRESS, SERVER_PORT))
+socket.listen()
 
-# Message received from socket
-client_message = b'''reg get
-HKCU\Bro,damn,,
-'''
+while True:
+    # Wait for client connection
+    conn, _ = socket.accept()
 
-(command, option, client_data) = Util.extract_message(client_message, message_type='client')
+    while True:
+        # Message received from socket
+        client_message = conn.recv(2 ** 16)
+        if client_message == b'':
+            break
 
-error_code = 0
-server_data = ""
+        # Extract the received message
+        (command, option, client_data) = Util.extract_message(client_message, message_type='client')
 
-# Check the command variable for task
-if command == 'screenshot':
-    (error_code, server_data) = svac.action_screenshot(option, client_data)
-elif command == 'process':
-    (error_code, server_data) = svac.action_process(option, client_data)
-elif command == 'app':
-    (error_code, server_data) = svac.action_app(option, client_data)
-elif command == 'keylogging':
-    (error_code, server_data) = svac.action_keylogging(option, client_data)
-elif command == 'reg':
-    (error_code, server_data) = svac.action_reg(option, client_data)
-elif command == 'shutdown':
-    (error_code, server_data) = svac.action_shutdown(option, client_data)
-else:
-    error_code = 3 # Unrecognized command
+        error_code = 0
+        server_data = ""
 
-# Look up for error message from predefined dictionary
-error_message = Util.error_message_dictionary[error_code]
+        # Check the command variable for task
+        if command == 'screenshot':
+            (error_code, server_data) = svac.action_screenshot(option, client_data)
+        elif command == 'process':
+            (error_code, server_data) = svac.action_process(option, client_data)
+        elif command == 'app':
+            (error_code, server_data) = svac.action_app(option, client_data)
+        elif command == 'keylogging':
+            (error_code, server_data) = svac.action_keylogging(option, client_data)
+        elif command == 'reg':
+            (error_code, server_data) = svac.action_reg(option, client_data)
+        elif command == 'shutdown':
+            (error_code, server_data) = svac.action_shutdown(option, client_data)
+        else:
+            error_code = 3 # Unrecognized command
 
-# Create message
-server_message = Util.package_message(error_code, error_message, server_data)
+        # Look up for error message from predefined dictionary
+        error_message = Util.error_message_dictionary[error_code]
 
-print(server_message.decode('utf-8'))
+        # Create message
+        server_message = Util.package_message(error_code, error_message, server_data)
+        print(server_message.decode('utf-8'))
+
+        # Send message
+        conn.send(server_message)
