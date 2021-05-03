cf7f0291464729118a590a35f24ab814256b6404
Nam Mai <mdnam2410@gmail.com>
2021-05-03 11:10:21 +0700

    Minor fix

--- Util.py
+++ Util.py
@@ -7,7 +7,7 @@ def extract_message(message: bytes, message_type='client') -> tuple:
     field1, field2 = tuple(header_field.split(' '))
 
     # field1 is Error code if this is a server message
-    if type == 'server':
+    if message_type == 'server':
         field1 = int(field1)
     
     return (field1, field2, data_field)
